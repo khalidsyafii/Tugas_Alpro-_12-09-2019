846ae20fe063c2fb952d3feab8e1cc9d3104cf66
khalidsyafii <54666377+khalidsyafii@users.noreply.github.com>
2019-09-12 08:46:06 +0700

    MUH KHALID ASY-SYAFII
    1201184178
    TI-42-11
    
    TUGAS ALPRO

---
+++ 1.py
@@ -0,0 +1,18 @@
+print("start")
+
+GPA = [3, 2.7, 2.5, 4]
+
+def perkalian():
+    total = index*bonus
+    return total
+
+bonus = 500000
+
+for index in GPA :
+    print(index)
+    if index > 3:
+        print(index,"anda mendapatkan bonus Rp.", perkalian())
+    elif index <= 3:
+        print(index,"anda tidak mendapatkan bonus")
+
+       
